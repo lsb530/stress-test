5083e6ce9067143a83cb5f21b4e03eaa5abf948d
seung.bok <seungbok.lee@huinnoaim.com>
2023-12-08 16:45:51 +0900

    readMe htop 추가

--- readMe.md
+++ readMe.md
@@ -1,11 +1,15 @@
-help) ps -ef | grep (Process Name) | grep -v grep | awk '{print $2}'
+help)
+brew install htop
+ps -ef | grep (Process Name) | grep -v grep | awk '{print $2}'
+htop -p $(ps -ef | grep (Process Name) | grep -v grep | awk '{print $2}')
 ```bash
 ps -ef | grep Stress 
 ps -ef | grep Stress | grep -v grep | awk '{print $2}'
 top -pid (ProcessId)
-
+가
 (final)
 top -pid $(ps -ef | grep "Stress" | grep -v grep | awk '{print $2}')
+htop -p $(ps -ef | grep "Stress" | grep -v grep | awk '{print $2}') 
 ```
 
 
--- src/main/kotlin/com/boki/stresstest/controller/StressController.kt
+++ src/main/kotlin/com/boki/stresstest/controller/StressController.kt
@@ -18,7 +18,7 @@ class StressController {
 
     @GetMapping("/cpu-intensive")
     fun cpuIntensiveTask(): String {
-        val n = 50000 // 범위 증가
+        val n = 10000 // 범위 증가
         for (i in 2 until n) {
             for (j in 2 until n) { // 추가적인 반복
                 isPrime(i * j) // 계산량 증가
@@ -27,19 +27,25 @@ class StressController {
         return "Completed CPU intensive task"
     }
 
-//    @GetMapping("/memory-intensive")
-//    fun memoryIntensiveTask(): String {
-//        val largeList = MutableList(100_000_000) { it }
-//        return "Created a large list in memory"
-//    }
-
     @GetMapping("/memory-intensive")
     fun memoryIntensiveTask(): String {
-        val largeList = MutableList(500_000_000) { it } // 리스트 크기 증가
-        val anotherLargeList = largeList.map { it.toDouble() } // 추가 리스트 생성
+        val largeList = MutableList(100_000_000) { it }
         return "Created a large list in memory"
     }
 
+//    @GetMapping("/memory-intensive")
+//    fun memoryIntensiveTask(): String {
+//        val largeList = MutableList(50_000_000) { it } // 리스트 크기 증가
+//        val anotherLargeList = largeList.map { it.toDouble() } // 추가 리스트 생성
+//        return "Created a large list in memory"
+//    }
+
+//    @GetMapping("/memory-intensive")
+//    fun memoryIntensiveTask(): String {
+//        val largeArray = ByteArray(1024 * 1024 * 1024) // 1GB 크기의 바이트 배열 생성
+//        // 배열을 참조하고 있는 동안 메모리에 유지
+//        return "Created a large array in memory. First element: ${largeArray[0]}"
+//    }
 
     private fun isPrime(n: Int): Boolean {
         for (i in 2 until n) {
